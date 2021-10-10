d48fb2fdf33141b9096411060a03846f6508a882
Tongze Wang <wtongze@gmail.com>
2021-10-10 17:21:56 +0800

    improve route page

--- src/pages/RoutePage.tsx
+++ src/pages/RoutePage.tsx
@@ -1,10 +1,12 @@
 import AppFrame from '../components/AppFrame';
 import {
   ChevronLeft as ChevronLeftIcon,
+  KeyboardArrowDown as KeyboardArrowDownIcon,
+  KeyboardArrowUp as KeyboardArrowUpIcon,
   Train as TrainIcon,
 } from '@mui/icons-material';
 import { useHistory, useParams } from 'react-router';
-import { Container, Grid, Typography, Chip } from '@mui/material';
+import { Container, Grid, Typography, Chip, Button } from '@mui/material';
 import { useEffect, useState } from 'react';
 import routeTest from './RouteTest.json';
 import { DirectionRoute } from '../type';
@@ -15,6 +17,7 @@ function RoutePage(props: ReduxProps) {
   const history = useHistory();
   const params = useParams<{ origin: string; destination: string }>();
   const [route, setRoute] = useState<DirectionRoute>();
+  const [hideList, setHideList] = useState<boolean[][]>([]);
 
   const { origin, destination } = params;
   const mode = 'all';
@@ -27,6 +30,26 @@ function RoutePage(props: ReduxProps) {
       console.log(origin, destination, fromTime, mode);
       if (route === undefined && subscribe) {
         setRoute(routeTest as DirectionRoute);
+
+        let total: boolean[][] = [];
+        for (let i = 0; i < routeTest.directions.length - 1; i++) {
+          const step = routeTest.directions[i];
+          if (step.type === 'TRAIN') {
+            if (step.via) {
+              const temp: boolean[] = [];
+              for (let j = 0; j < step.via.length; j++) {
+                temp.push(true);
+              }
+              total.push(temp);
+            } else {
+              total.push([]);
+            }
+          } else {
+            total.push([]);
+          }
+        }
+        setHideList(total);
+        console.log(total);
       }
     }, 1000);
 
@@ -48,11 +71,11 @@ function RoutePage(props: ReduxProps) {
       >
         <Container sx={{ padding: 4 }}>
           <Typography variant='h5'>Route Page</Typography>
-          {route?.directions.map((r, i) => {
+          {route?.directions.map((r, stepIndex) => {
             if (r.type === 'START_TRANSFER') {
               const station = props.stations.find((i) => i.id === r.from);
               return (
-                <div className='start-transfer' key={i}>
+                <div className='start-transfer' key={stepIndex}>
                   <Grid container sx={{ mt: 4 }}>
                     <Grid item xs={2} textAlign='center'>
                       <Typography fontSize={20} fontWeight={'bold'}>
@@ -120,7 +143,7 @@ function RoutePage(props: ReduxProps) {
               );
 
               return (
-                <div className='train' key={i}>
+                <div className='train' key={stepIndex}>
                   <Grid container sx={{ mt: 4 }}>
                     <Grid item xs={2} textAlign='center'>
                       <Typography fontSize={20} fontWeight={'bold'}>
@@ -217,6 +240,123 @@ function RoutePage(props: ReduxProps) {
                       ) : null}
                     </Grid>
                   </Grid>
+                  {r.via.map((v, viaIndex) => {
+                    const vRailway = props.railways.find(
+                      (i) => i.id === v.railwayId
+                    );
+
+                    const vNextRailway = props.railways.find((i) =>
+                      viaIndex < r.via.length - 1
+                        ? i.id === r.via[viaIndex + 1].railwayId
+                        : false
+                    );
+
+                    let hide = true;
+                    if (
+                      hideList.length > 0 &&
+                      hideList[stepIndex] !== undefined &&
+                      hideList[stepIndex][viaIndex] !== undefined
+                    ) {
+                      hide = hideList[stepIndex][viaIndex];
+                    }
+
+                    return v.stops.map((s, stopIndex) => {
+                      const stop = props.stations.find((i) => i.id === s);
+
+                      return (
+                        <div key={stepIndex + ' ' + viaIndex + '-' + stopIndex}>
+                          {stopIndex === 0 ? (
+                            <Grid container>
+                              <Grid item xs={2} />
+                              <Grid item xs={1}>
+                                <div
+                                  style={{
+                                    backgroundColor: vRailway?.color || 'grey',
+                                    width: '16px',
+                                    height: '100%',
+                                    margin: '0 auto',
+                                  }}
+                                ></div>
+                              </Grid>
+                              <Grid item xs={9} sx={{ px: 4, py: 3 }}>
+                                <Button
+                                  size='small'
+                                  variant='text'
+                                  endIcon={
+                                    hide ? (
+                                      <KeyboardArrowDownIcon />
+                                    ) : (
+                                      <KeyboardArrowUpIcon />
+                                    )
+                                  }
+                                  onClick={() => {
+                                    const tempList = [...hideList];
+                                    tempList[stepIndex][viaIndex] =
+                                      !hideList[stepIndex][viaIndex];
+                                    setHideList(tempList);
+                                  }}
+                                >
+                                  {hide ? 'Show More' : 'Show Less'}
+                                </Button>
+                              </Grid>
+                            </Grid>
+                          ) : null}
+                          {hide ? null : (
+                            <Grid container>
+                              <Grid item xs={2} />
+                              <Grid item xs={1}>
+                                <div
+                                  style={{
+                                    backgroundColor: vRailway?.color || 'grey',
+                                    width: '16px',
+                                    height: '100%',
+                                    margin: '0 auto',
+                                  }}
+                                ></div>
+                              </Grid>
+                              <Grid item xs={9} sx={{ px: 4, py: 3 }}>
+                                <Typography>{stop?.title?.en}</Typography>
+                                <Typography>{stop?.stationCode}</Typography>
+                              </Grid>
+                            </Grid>
+                          )}
+                          {stopIndex === v.stops.length - 1 &&
+                          fromRailway?.id !== toRailway?.id ? (
+                            <Grid container key={viaIndex + '-' + stopIndex}>
+                              <Grid item xs={2}></Grid>
+                              <Grid item xs={1}>
+                                <div
+                                  style={{
+                                    backgroundColor: vRailway?.color || 'grey',
+                                    width: '16px',
+                                    height: '50%',
+                                    margin: '0 auto',
+                                  }}
+                                ></div>
+                                <div
+                                  style={{
+                                    backgroundColor:
+                                      viaIndex === r.via.length - 1
+                                        ? toRailway?.color
+                                        : vNextRailway?.color || 'grey',
+                                    width: '16px',
+                                    height: '50%',
+                                    margin: '0 auto',
+                                  }}
+                                ></div>
+                              </Grid>
+                              <Grid item xs={9} sx={{ px: 4, py: 3 }}>
+                                <Typography>
+                                  Direct service to {vRailway?.title?.en} via{' '}
+                                  {stop?.title?.en}
+                                </Typography>
+                              </Grid>
+                            </Grid>
+                          ) : null}
+                        </div>
+                      );
+                    });
+                  })}
                   <Grid container>
                     <Grid item xs={2} textAlign='center' alignSelf={'flex-end'}>
                       <Typography fontSize={20} fontWeight={'bold'}>
@@ -269,7 +409,7 @@ function RoutePage(props: ReduxProps) {
             }
             if (r.type === 'TRANSFER') {
               return (
-                <div className='transfer' key={i}>
+                <div className='transfer' key={stepIndex}>
                   <Grid container sx={{ mt: 4 }}>
                     <Grid item xs={2}></Grid>
                     <Grid item xs={1}>
@@ -299,7 +439,7 @@ function RoutePage(props: ReduxProps) {
             if (r.type === 'END_TRANSFER') {
               const station = props.stations.find((i) => i.id === r.to);
               return (
-                <div className='end-transfer' key={i}>
+                <div className='end-transfer' key={stepIndex}>
                   <Grid container sx={{ mt: 4 }}>
                     <Grid item xs={2} textAlign='center' alignSelf='flex-end'>
                       <Typography fontSize={20} fontWeight={'bold'}>
@@ -317,7 +457,7 @@ function RoutePage(props: ReduxProps) {
                         }}
                       ></div>
                     </Grid>
-                    <Grid item xs={9} sx={{ px: 4 }} >
+                    <Grid item xs={9} sx={{ px: 4 }}>
                       <Typography
                         fontWeight={'medium'}
                         fontSize={18}
